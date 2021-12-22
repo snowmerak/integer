48566f5fb8fd964bf4e63cc916ba7a83063083f6
snowmerak <snowmerak@outlook.com>
2021-12-22 22:12:51 +0900

    create big size integer

---
+++ lib/src/i256.dart
@@ -0,0 +1,122 @@
+import 'package:primitive/src/number.dart';
+
+class I256 extends BigInteger {
+  I256(BigInt init) : super(init) {
+    value = BigInt.parse(init.toRadixString(10), radix: 10);
+    while (value > BigInteger.I256MAX) {
+      value -= BigInteger.U256DIST;
+    }
+    while (value < BigInteger.I256MIN) {
+      value += BigInteger.U256DIST;
+    }
+  }
+
+  @override
+  I256 operator %(BigInteger other) {
+    return I256(value % other.value);
+  }
+
+  @override
+  I256 operator &(BigInteger other) {
+    return I256(value & other.value);
+  }
+
+  @override
+  I256 operator *(BigInteger other) {
+    return I256(value * other.value);
+  }
+
+  @override
+  I256 operator +(BigInteger other) {
+    return I256(value + other.value);
+  }
+
+  @override
+  I256 operator -() {
+    return I256(-value);
+  }
+
+  @override
+  I256 operator -(BigInteger other) {
+    return I256(value - other.value);
+  }
+
+  @override
+  I256 operator /(BigInteger other) {
+    return I256(value ~/ other.value);
+  }
+
+  @override
+  bool operator <(BigInteger other) {
+    return value < other.value;
+  }
+
+  @override
+  I256 operator <<(int shiftAmount) {
+    return I256(value << shiftAmount);
+  }
+
+  @override
+  bool operator <=(BigInteger other) {
+    return value <= other.value;
+  }
+
+  @override
+  bool operator >(BigInteger other) {
+    return value > other.value;
+  }
+
+  @override
+  bool operator >=(BigInteger other) {
+    return value >= other.value;
+  }
+
+  @override
+  I256 operator >>(int shiftAmount) {
+    return I256(value >> shiftAmount);
+  }
+
+  @override
+  I256 operator ^(BigInteger other) {
+    return I256(value ^ other.value);
+  }
+
+  @override
+  I256 abs() {
+    return I256(value.abs());
+  }
+
+  @override
+  I256 pow(BigInteger exponent) {
+    return I256(value.pow(exponent.value.toInt()));
+  }
+
+  @override
+  I256 rotateLeft(int shiftAmount) {
+    var temp = BigInt.parse(value.toRadixString(2), radix: 2);
+    var result = temp << shiftAmount | temp >> (256 - shiftAmount);
+    return I256(result);
+  }
+
+  @override
+  I256 rotateRight(int shiftAmount) {
+    var temp = BigInt.parse(value.toRadixString(2), radix: 2);
+    var result = temp >> shiftAmount | temp << (256 - shiftAmount);
+    return I256(result);
+  }
+
+  @override
+  I256 operator |(BigInteger other) {
+    return I256(value | other.value);
+  }
+
+  @override
+  I256 operator ~() {
+    return I256(value.toUnsigned(256).toSigned(256));
+  }
+
+  @override
+  I256 operator ~/(BigInteger other) {
+    return I256(value ~/ other.value);
+  }
+}
---
+++ lib/src/u128.dart
@@ -0,0 +1,122 @@
+import 'package:primitive/src/number.dart';
+
+class U128 extends BigInteger {
+  U128(BigInt init) : super(init) {
+    value = BigInt.parse(init.toRadixString(10), radix: 10);
+    while (value > BigInteger.U128MAX) {
+      value -= BigInteger.U128DIST;
+    }
+    while (value < BigInteger.U128MIN) {
+      value += BigInteger.U128DIST;
+    }
+  }
+
+  @override
+  U128 operator %(BigInteger other) {
+    return U128(value % other.value);
+  }
+
+  @override
+  U128 operator &(BigInteger other) {
+    return U128(value & other.value);
+  }
+
+  @override
+  U128 operator *(BigInteger other) {
+    return U128(value * other.value);
+  }
+
+  @override
+  U128 operator +(BigInteger other) {
+    return U128(value + other.value);
+  }
+
+  @override
+  U128 operator -() {
+    return U128(-value);
+  }
+
+  @override
+  U128 operator -(BigInteger other) {
+    return U128(value - other.value);
+  }
+
+  @override
+  U128 operator /(BigInteger other) {
+    return U128(value ~/ other.value);
+  }
+
+  @override
+  bool operator <(BigInteger other) {
+    return value < other.value;
+  }
+
+  @override
+  U128 operator <<(int shiftAmount) {
+    return U128(value << shiftAmount);
+  }
+
+  @override
+  bool operator <=(BigInteger other) {
+    return value <= other.value;
+  }
+
+  @override
+  bool operator >(BigInteger other) {
+    return value > other.value;
+  }
+
+  @override
+  bool operator >=(BigInteger other) {
+    return value >= other.value;
+  }
+
+  @override
+  U128 operator >>(int shiftAmount) {
+    return U128(value >> shiftAmount);
+  }
+
+  @override
+  U128 operator ^(BigInteger other) {
+    return U128(value ^ other.value);
+  }
+
+  @override
+  U128 abs() {
+    return U128(value.abs());
+  }
+
+  @override
+  U128 pow(BigInteger exponent) {
+    return U128(value.pow(exponent.value.toInt()));
+  }
+
+  @override
+  U128 rotateLeft(int shiftAmount) {
+    var temp = BigInt.parse(value.toRadixString(2), radix: 2);
+    var result = temp << shiftAmount | temp >> (128 - shiftAmount);
+    return U128(result);
+  }
+
+  @override
+  U128 rotateRight(int shiftAmount) {
+    var temp = BigInt.parse(value.toRadixString(2), radix: 2);
+    var result = temp >> shiftAmount | temp << (128 - shiftAmount);
+    return U128(result);
+  }
+
+  @override
+  U128 operator |(BigInteger other) {
+    return U128(value | other.value);
+  }
+
+  @override
+  U128 operator ~() {
+    return U128(value.toUnsigned(128).toSigned(128));
+  }
+
+  @override
+  U128 operator ~/(BigInteger other) {
+    return U128(value ~/ other.value);
+  }
+}
