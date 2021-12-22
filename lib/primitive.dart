aa646815b5a558172870bf295542788551d37d4f
snowmerak <snowmerak@outlook.com>
2021-12-22 21:49:03 +0900

    Create primitive.dart

---
+++ lib/primitive.dart
@@ -0,0 +1,16 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library primitive;
+
+export 'src/i8.dart';
+export 'src/i16.dart';
+export 'src/i32.dart';
+export 'src/i64.dart';
+export 'src/u8.dart';
+export 'src/u16.dart';
+export 'src/u32.dart';
+export 'src/u64.dart';
+
+
+// TODO: Export any libraries intended for clients of this package.
