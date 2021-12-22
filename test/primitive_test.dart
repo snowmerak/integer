66ca8da30c7a51f7f71cb19e07a50ae0199111ce
snowmerak <snowmerak@outlook.com>
2021-12-22 21:48:14 +0900

    init package

---
+++ test/primitive_test.dart
@@ -0,0 +1,14 @@
+import 'package:primitive/primitive.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(isFalse, isTrue);
+    });
+  });
+}
